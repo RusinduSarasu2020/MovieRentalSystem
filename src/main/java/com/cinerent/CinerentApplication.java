de85bd043ee2ce6eddba9321bc9e63e99d8f86db
RusinduSarasu2020 <rusindusarasu123@gmail.com>
2026-05-05 20:56:37 +0530

    Set-up initial Spring Boot,Maven project structure and files

---
+++ src/main/java/com/cinerent/CinerentApplication.java
@@ -0,0 +1,12 @@
+package com.cinerent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinerentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CinerentApplication.class, args);
+        
+    }
+}
