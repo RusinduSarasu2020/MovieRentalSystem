3fceeadbcff35469cb73d4023862ac4708b77d6e
it25100920 <it25100920@my.sliit.lk>
2026-05-17 17:14:43 +0530

    Update RentalService.java

--- src/main/java/com/cinerent/service/RentalService.java
+++ src/main/java/com/cinerent/service/RentalService.java
@@ -1,34 +1,58 @@
 package com.cinerent.service;
 
-import com.cinerent.model.*;
+import com.cinerent.model.Movie;
+import com.cinerent.model.Rental;
+import com.cinerent.model.User;
 import com.cinerent.repository.RentalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class RentalService {
-    @Autowired private RentalRepository repo;
-    @Autowired private MovieService movieService;
 
-    public Rental rentMovie(User u, Movie m) {
-        m.setAvailable(false);
-        movieService.update(m);
-        return repo.save(new Rental(u, m));
+    @Autowired
+    private RentalRepository repo;
+
+    @Autowired
+    private MovieService movieService;
+
+    public Rental rentMovie(User user, Movie movie) {
+        movie.setAvailable(false);
+        movieService.update(movie);
+
+        return repo.save(new Rental(user, movie));
     }
+
     public Rental returnMovie(Long rentalId) {
-        Rental r = repo.findById(rentalId).orElseThrow();
-        r.setReturnDate(LocalDate.now());
-        r.setStatus("RETURNED");
-        r.calculateLateFee();
-        Movie m = r.getMovie();
-        m.setAvailable(true);
-        movieService.update(m);
-        return repo.save(r);
+        Rental rental = repo.findById(rentalId).orElseThrow();
+
+        rental.setReturnDate(LocalDate.now());
+        rental.setStatus("RETURNED");
+        rental.calculateLateFee();
+
+        Movie movie = rental.getMovie();
+        movie.setAvailable(true);
+        movieService.update(movie);
+
+        return repo.save(rental);
     }
-    public List<Rental> all() { return repo.findAll(); }
-    public List<Rental> byUser(User u) { return repo.findByUser(u); }
-    public long activeCount() { return repo.findByStatus("ACTIVE").size(); }
-    public long returnedCount() { return repo.findByStatus("RETURNED").size(); }
+
+    public List<Rental> all() {
+        return repo.findAll();
+    }
+
+    public List<Rental> byUser(User user) {
+        return repo.findByUser(user);
+    }
+
+    public long activeCount() {
+        return repo.findByStatus("ACTIVE").size();
+    }
+
+    public long returnedCount() {
+        return repo.findByStatus("RETURNED").size();
+    }
 }
